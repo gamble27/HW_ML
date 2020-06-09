0ab669234bf1f5a62e69f44ba576bbea0e9cb451
gamble27 <olyushkam@gmail.com>
2020-06-10 00:41:04 +0300

    Lab 4 - PMF     + updated algorithm to make it faster     + added l4.py, which works with modified algorithm

--- lab4/l4.py
+++ lab4/l4.py
@@ -1,12 +1,6 @@
 import numpy as np
+import pandas as pd
 from tqdm import trange
-
-
-def fro(t: np.ndarray) -> float:
-    """
-    Frobenius norm of the matrix
-    """
-    return np.linalg.norm(t, ord='fro')
 
 
 def PMF(X: np.ndarray, d: int, l: float, s: float = 1,
@@ -53,13 +47,13 @@ def PMF(X: np.ndarray, d: int, l: float, s: float = 1,
         # calculate U
         for i in trange(n):
             u[:, i] = ((1/(l*s**2 + np.array(
-                    [ind[i, j] * fro(v[:, j].reshape(d, 1)) ** 2 for j in Omega_u[i]]
+                    [ind[i, j] * np.linalg.norm(v[:, j].reshape(d, 1), ord='fro') ** 2 for j in Omega_u[i]]
             ).sum()) * (v @ (ind * X)[i, :].reshape((1, m)).T)).reshape((d,)))
 
         # calculate V
         for j in trange(m):
             v[:, j] = ((1/(l*s**2 + np.array(
-                    [ind[i, j] * fro(u[:, i].reshape(d, 1)) ** 2 for i in Omega_v[j]]
+                    [ind[i, j] * np.linalg.norm(u[:, i].reshape(d, 1), ord='fro') ** 2 for i in Omega_v[j]]
             ).sum()) * (u @ (ind * X)[:, j].reshape((1, n)).T)).reshape((d,)))
 
         # save this for later
@@ -67,17 +61,48 @@ def PMF(X: np.ndarray, d: int, l: float, s: float = 1,
         # np.save('/home/olga/Projects/HW_ML/lab4/experiment/v.npy', v)
 
         # compute cost
-        cost = ((s**(-2)) * fro(ind*(X - u.T @ v))**2 + l * fro(u)**2 + l * fro(v)**2) / 2
+        cost = ((s**(-2)) * np.linalg.norm(ind*(X - u.T @ v), ord='fro')**2 + l * np.linalg.norm(u, ord='fro')**2 + l * np.linalg.norm(v, ord='fro')**2) / 2
         if print_cost and (k + 1) % print_cost == 0:
             print(f"{k+1} iteration cost: {np.log(cost):.5f}")
 
     return u, v
 
 
-if __name__ == "__main__":
-    # test case
-    x = np.zeros((10, 20))
-    x[4,5] = x[3,2] = x[0,15] = x[7,13] = 1
-    x[6,8] = x[9,4] = x[4,17] = x[2,19] = 4
-    U,V = PMF(x, d=3, l=.4, s=2, max_iter=10, print_cost=1)
-    print((U.T @ V)[6,8])
+# get data
+df = pd.read_table("~/Projects/HW_ML/data/data-filtering/user_artists.dat")
+
+# Approach 1. Work with rounded ln(X)
+N, M = 1832, 17632
+N1 = df['userID'].max()
+M1 = df['artistID'].max()
+
+X = np.zeros((N1, M1))
+for _, row in df.iterrows():
+    X[row['userID']-1, row['artistID']-1] = np.round(np.log(row['weight']))
+
+# X[:20, 40:60] - validation set
+Y = []
+for i in range(20):
+    for j in range(40, 60):
+        if X[i,j] > 0:
+            Y.append((i,j,X[i,j]))
+
+# Y = np.array(Y)
+X[:20, 40:60] = np.zeros(X[:20, 40:60].shape)
+
+# np.save('/home/olga/Projects/HW_ML/lab4/experiment/x.npy', X)
+# np.save('/home/olga/Projects/HW_ML/lab4/experiment/y.npy', np.array(Y))
+
+print('beep')
+# U = np.load('/home/olga/Projects/HW_ML/lab4/experiment/u.npy')
+# V = np.load('/home/olga/Projects/HW_ML/lab4/experiment/v.npy')
+U, V = PMF(X, d=100, l=2, s=1, max_iter=5, print_cost=1)#,
+           # pretrained_u=U, pretrained_v=V)
+
+np.save('/lab4/u.npy', U)
+np.save('/lab4/v.npy', V)
+X_hat = U.T @ V
+
+print("pred vs true")
+for i, j, x_true in Y:
+    print(f"{X_hat[i,j]:.2f}    {x_true}")
